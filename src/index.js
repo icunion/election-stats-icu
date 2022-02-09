c2f04ce372a689a01b4d61cdf86cc7ece42d0d83
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-09 13:07:28 +0000

    styles: Setup scss and add initial global styles and variables [refs #3927]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import styles from './styles.module.css'
 
+import './styles/common.scss'
 import config from './config'
 
 export { default as Banner } from './blocks/Banner'
