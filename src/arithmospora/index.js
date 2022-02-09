b06ae0d79e741d5cf5c50ad6b64163f2dd1c457a
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-09 13:03:00 +0000

    arithmospora: Tidy up and document index and store [refs #3927]

--- src/arithmospora/index.js
+++ src/arithmospora/index.js
@@ -2,26 +2,42 @@ import { arithmosporaActions } from './slice'
 import { store } from './store'
 import config from '../config'
 
-let connected = false
-let sockets = []
+/**
+ * Object to hold our stats source WebSocket instances.
+ * @type {Object.<string, WebSocket>}
+ */
+let sockets = {}
 
-const connectSources = (statsSources = [], dispatch = () => {}) => {
-  // Don't reconnect if already connected
-  if (connected) {
-    return
-  }
-  connected = true
+/**
+ * Connect a set of stats sources, ensuring the state object is prepared for
+ * each source. We only connect a source if it doesn't already exist in state,
+ * which allows us to call this from our components every time they render
+ * and ensure we only actually connect the sockets once for each source.
+ * @param {string[]} statsSources - Array of stats source names corresponding
+ *     to sources defined in config.
+ * @see ../config
+ */
+const connectSources = (statsSources = []) => {
+  const state = store.getState()
 
   for (const statsSource of statsSources) {
     if (!(statsSource in config.sources)) {
       console.log('Source not found in configuration', { statsSource })
       continue
     }
-    store.dispatch(arithmosporaActions.addSource(statsSource))
-    connectSource(statsSource)
+    if (!(statsSource in state.stats.sources)) {
+      store.dispatch(arithmosporaActions.addSource(statsSource))
+      connectSource(statsSource)
+    }
   }
 }
 
+/**
+ * Connects a websocket for the named stats source, ensuring the socket gets
+ * reconnected automatically if it becomes disconnected. Sets up an event
+ * listener on the websocket to forward arithmospora events to the reducer.
+ * @param {string} statsSource - The name of the stats source to connect.
+ */
 const connectSource = (statsSource) => {
   const sourceConf = config.sources[statsSource]
   let interval = null
--- src/arithmospora/store.js
+++ src/arithmospora/store.js
@@ -1,12 +1,22 @@
 import arithmosporaReducer from './slice'
 import { configureStore } from '@reduxjs/toolkit'
 
+/**
+ * We keep a reference to the store here to allow us to access it
+ * independently, i.e. in our socket event dispatcher.
+ */
 let store
 
-const configureArithmosporaStore = (config) => {
-  let { reducer = {} } = config
+/**
+ * Wrapper for configureStore which adds in our reducer and allows us to keep
+ * a reference to the created store object.
+ * @param {Object} args - The data structure to pass to configureStore.
+ * @returns {Object} - The newly created Redux store.
+ */
+const configureArithmosporaStore = (args) => {
+  let { reducer = {} } = args
   reducer.stats = arithmosporaReducer
-  store = configureStore({ ...config, reducer })
+  store = configureStore({ ...args, reducer })
   return store
 }
 
