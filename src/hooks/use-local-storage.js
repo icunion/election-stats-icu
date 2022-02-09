bac3e330f1fe29dc6221d71918bba876aa15e0ab
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-09 17:50:44 +0000

    hooks: Add useLocalStorage hook [refs #3927]

---
+++ src/hooks/use-local-storage.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import ls from 'localstorage-slim'
+
+function getStorageValue(key, defaultValue, config) {
+  const value = ls.get(key, config)
+  return value !== null ? value : defaultValue
+}
+
+const useLocalStorage = (key, defaultValue, setterConfig = {}, getterConfig = {}) => {
+  const [value, setValue] = useState(() => {
+    return getStorageValue(key, defaultValue, getterConfig)
+  })
+
+  useEffect(() => {
+    ls.set(key, value, setterConfig)
+  }, [key, value, setterConfig])
+
+  return [value, setValue]
+}
+
+export default useLocalStorage
