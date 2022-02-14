47db542395476385f1917e2472eb331258223c10
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-14 17:17:40 +0000

    index: Export DashboardLE2022 [refs #3927]

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './styles/common.scss'
 import config from './config'
 
 export { default as Banner } from './blocks/Banner'
+export { default as DashboardLE2022 } from './pages/le2022/Dashboard'
 export { default as configureStore } from './arithmospora/store'
