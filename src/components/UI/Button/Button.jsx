7ce03c8acacacdb011cce9b937f2302a4c96c3f9
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-14 16:34:04 +0000

    components/UI/Button: Add type prop to allow rendering buttons as buttons [refs #3927]

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -3,15 +3,27 @@ import React from 'react'
 import styles from './Button.module.scss'
 
 const Button = (props) => {
-  return (
-    <a
-      className={`${styles.button} ${styles[props.className] || ''}`}
-      href={props.href}
-      target={props.target}
-    >
-      {props.children}
-    </a>
-  )
+  if (props.type == 'button') {
+    return (
+      <button
+        className={`${styles.button} ${styles[props.className] || ''}`}
+        type="button"
+        onClick={props.onClick}
+      >
+        {props.children}
+      </button>
+    )
+  } else {
+    return (
+      <a
+        className={`${styles.button} ${styles[props.className] || ''}`}
+        href={props.href}
+        target={props.target}
+      >
+        {props.children}
+      </a>
+    )
+  }
 }
 
 export default Button
