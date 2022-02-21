07ff2185cc6f38892375d3fb740daa1a26f6496c
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-21 17:32:06 +0000

    hooks: Add useD3 hook and create minimal d3 importer [refs #3927]

---
+++ src/d3-import/index.js
@@ -0,0 +1,17 @@
+import { max, min } from 'd3-array'
+import { axisBottom, axisRight } from 'd3-axis'
+import { format } from 'd3-format'
+import { scaleBand, scaleLinear, scaleOrdinal } from 'd3-scale'
+import { select } from 'd3-selection'
+
+export default {
+  axisBottom,
+  axisRight,
+  format,
+  max,
+  min,
+  scaleBand,
+  scaleLinear,
+  scaleOrdinal,
+  select
+}
