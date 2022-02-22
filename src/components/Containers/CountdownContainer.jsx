6dac13d49e4f1a8ce4c09b555dc52d190d97d069
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-22 15:07:48 +0000

    components/Containers/CountdownContainer: Add completed class to parent element; reformat code; adjust styles [refs #3927]

--- src/components/Containers/CountdownContainer.jsx
+++ src/components/Containers/CountdownContainer.jsx
@@ -6,38 +6,43 @@ import Button from '../UI/Button/Button'
 import styles from './CountdownContainer.module.scss'
 
 const CountdownContainer = (props) => {
-    // Use a state property to determine whether the countdown has completed.
-    // If the close date is already in the past, we want the state to be set
-    // to true from the outset.
-    const [countdownCompleted, setCountdownCompleted] = useState(
-        props.votingCloseDate < Date.now()
-    )
+  // Use a state property to determine whether the countdown has completed.
+  // If the close date is already in the past, we want the state to be set
+  // to true from the outset.
+  const [countdownCompleted, setCountdownCompleted] = useState(
+    props.votingCloseDate < Date.now()
+  )
 
-    const dashboardCountdownCompleteHandler = () => {
-        setCountdownCompleted(true)
-    }
+  const dashboardCountdownCompleteHandler = () => {
+    setCountdownCompleted(true)
+  }
 
-    return (
-        <div className={styles.countdownContainer}>
-             <ElectionCountDown
-                date={props.votingCloseDate}
-                onComplete={dashboardCountdownCompleteHandler}
-                className='dashboardCountdown'
-            >
-                <p className={styles.countdownCompleted}>Thank you for voting!</p>
-            </ElectionCountDown>   
-            {!countdownCompleted && (
-                <div className='text-center'>
-                <Button href='https://vote.union.ic.ac.uk' target='_blank' className="vote">
-                    VOTE
-                </Button>
-                </div>
-            )}
-
-            
+  return (
+    <div
+      className={`${styles.countdownContainer} ${
+        countdownCompleted ? styles.completed : ''
+      }`}
+    >
+      <ElectionCountDown
+        date={props.votingCloseDate}
+        onComplete={dashboardCountdownCompleteHandler}
+        className='dashboardCountdown'
+      >
+        <p className={styles.countdownCompleted}>Thank you for voting!</p>
+      </ElectionCountDown>
+      {!countdownCompleted && (
+        <div className='text-center'>
+          <Button
+            href='https://vote.union.ic.ac.uk'
+            target='_blank'
+            className='vote'
+          >
+            VOTE
+          </Button>
         </div>
-        
-    )
+      )}
+    </div>
+  )
 }
 
-export default CountdownContainer;
+export default CountdownContainer
