80dc25794318bd7a3a77a03a2fba31c0d9d98056
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-24 12:27:49 +0000

    definitions: Add display names for departments, halls, and CUs [refs #3927]

---
+++ src/definitions/constituent-unions.js
@@ -0,0 +1,6 @@
+export const displayNames = {
+  'CGCU': 'City and Guilds College',
+  'ICSMSU': 'Imperial College School of Medicine',
+  'RCSU': 'Royal College of Science',
+  'RSMU': 'Royal School of Mines'
+}
