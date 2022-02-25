9a54986bed21cae005e60b6b39a5349ac65e1b2d
Phil Stewart <p.stewart@imperial.ac.uk>
2022-02-25 14:41:08 +0000

    arithmospora/store: Sanitize action for timed stats in devtools to reduce memory footprint [refs #3927]

--- src/arithmospora/store.js
+++ src/arithmospora/store.js
@@ -14,7 +14,7 @@ let store
  * @returns {Object} - The newly created Redux store.
  */
 const configureArithmosporaStore = (args) => {
-  let { reducer = {}, middleware } = args
+  let { reducer = {}, middleware, devTools } = args
   reducer.stats = arithmosporaReducer
   if (!middleware) {
     middleware = (getDefaultMiddleware) =>
@@ -22,7 +22,44 @@ const configureArithmosporaStore = (args) => {
         serializableCheck: false
       })
   }
-  store = configureStore({ ...args, reducer, middleware })
+  if (!devTools) {
+    // Devtools optimisation: don't include large time bucket objects in
+    // devTools actions. See
+    // https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/Troubleshooting.md#excessive-use-of-memory-and-cpu
+    devTools = {
+      actionSanitizer: (action) =>
+        action.type === 'arithmospora/stats' &&
+        action.payload.args[0] === 'timed'
+          ? {
+              ...action,
+              payload: {
+                ...action.payload,
+                payload: {
+                  ...action.payload.payload,
+                  dataPoints: {
+                    60: {
+                      name: '60',
+                      data: '<<TIME BUCKETS OBJECT>>',
+                      dataPoints: {}
+                    },
+                    300: {
+                      name: '60',
+                      data: '<<TIME BUCKETS OBJECT>>',
+                      dataPoints: {}
+                    },
+                    3600: {
+                      name: '60',
+                      data: '<<TIME BUCKETS OBJECT>>',
+                      dataPoints: {}
+                    }
+                  }
+                }
+              }
+            }
+          : action
+    }
+  }
+  store = configureStore({ ...args, reducer, middleware, devTools })
   return store
 }
 
