e3d8b7cd9f381fac14c8d15ad7563ab6cbc0c428
resapascual <resapascual@gmail.com>
2022-02-28 17:51:50 +0000

    components/Stats/TopTurnout: amended styling [refs #3927]

--- src/components/Stats/TopTurnoutList/TopTurnoutEntry.jsx
+++ src/components/Stats/TopTurnoutList/TopTurnoutEntry.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import AnimatedNumber from 'animated-number-react'
 
+import styles from './TopTurnoutEntry.module.scss'
+
 const TopTurnoutEntry = (props) => {
   return (
-    <li>
-      <span>{props.name}</span>{' '}
-      <AnimatedNumber
-        duration='750'
-        formatValue={(value) => `${value.toFixed(1)}%`}
-        value={props.percentage}
-      />
+    <li className={styles.entry}>
+
+        <div className={styles.label}>
+          <span>{props.name}</span>{' '}
+        </div>
+        <div className={styles.percentage}>
+          <AnimatedNumber
+            duration='750'
+            formatValue={(value) => `${value.toFixed(1)}%`}
+            value={props.percentage}
+          />
+        </div>
+
     </li>
   )
 }
--- src/components/Stats/TopTurnoutList/TopTurnoutList.jsx
+++ src/components/Stats/TopTurnoutList/TopTurnoutList.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { useSources, useProportionStat } from '../../../arithmospora/hooks'
+import { useSources, useRollingStat } from '../../../arithmospora/hooks'
 
 import TopTurnoutEntry from './TopTurnoutEntry'
+import RollingTimeFrame from '../../UI/Button/RollingTimeFrame'
 
 import { getListSortedByTurnout, top } from './top-turnout-helpers'
 import styles from './TopTurnoutList.module.scss'
@@ -11,19 +12,32 @@ const TopTurnoutList = (props) => {
   // Ensure stat sources get connected
   useSources([props.source])
 
-  const statsData = useProportionStat(
+  const [timeframe, setSelectedTimeFrame] = useState('total');
+
+  const RollingTimeFrameHandler = (selectedTimeFrame) => {
+    setSelectedTimeFrame(selectedTimeFrame) ;
+  };
+
+  const interval = timeframe;
+
+  const statsData = useRollingStat(
     props.source,
+    interval,
     props.stat,
     getListSortedByTurnout(props.stat)
   )
 
   return (
     <div className={styles.container}>
-      <ol>
+      <ol className={styles.entrylist}>
         {top(statsData, 5, props.stat).map(item => (
           <TopTurnoutEntry key={item.id} {...item} />
         ))}
       </ol>
+      <div className={styles.buttonsContainer}>
+        <RollingTimeFrame setActive={RollingTimeFrameHandler}/>
+        {/* <RollingTimeFrame /> */}
+      </div>
     </div>
   )
 }
