d02dc89ca1c527107ce2693d7f1464344f6232bb
Phil Stewart <p.stewart@imperial.ac.uk>
2022-03-07 12:09:59 +0000

    definitions: Add le2021 progress defs [refs #3927]

--- src/definitions/election-progress.js
+++ src/definitions/election-progress.js
@@ -1,4 +1,10 @@
 export const markers = {
+  'le2021': [
+    { name: '2016', percentage: 45.47, description: '2016: 7966 voters for all positions, 45.47% turnout' },
+    { name: '2019', percentage: 41.23, description: '2019: 8121 voters for all positions, 41.23% turnout' },
+    { name: '2020', percentage: 29.57, description: '2020: 6104 voters for all positions, 29.57% turnout' },
+    { name: 'UCL', percentage: 19.58, description: 'UCL (2021): 9473 voters, 19.58% turnout'}
+  ],
   'le2022': [
     { name: '2016', percentage: 45.47, description: '2016: 7966 voters for all positions, 45.47% turnout' },
     { name: '2019', percentage: 41.23, description: '2019: 8121 voters for all positions, 41.23% turnout' },
