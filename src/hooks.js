09a975fa2528570df732ac4aa2235d5cd74dad0b
Phil Stewart <p.stewart@imperial.ac.uk>
2022-03-08 15:48:39 +0000

    hooks: Export all hooks to facilitate use in apps [refs #3927]

---
+++ src/hooks.js
@@ -0,0 +1,5 @@
+export { default as useD3 } from './hooks/use-d3'
+export { default as useLocalStorage } from './hooks/use-local-storage'
+export { default as usePrevious } from './hooks/use-previous'
+
+export * from './arithmospora/hooks'
