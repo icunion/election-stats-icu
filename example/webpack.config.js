181b67551878fef2e82ddc2960c56f5037b50670
Phil Stewart <p.stewart@imperial.ac.uk>
2023-03-01 11:02:12 +0000

    package: Replace microbundle-crl with rollup for main library build, react-scripts with webpack for example app build [refs #4055]

---
+++ example/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.png|svg/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "build/"),
+    filename: "main.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
+  }
+};
---
+++ rollup.config.mjs
@@ -0,0 +1,51 @@
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import replace from '@rollup/plugin-replace';
+import smartAsset from "rollup-plugin-smart-asset"
+import styles from "rollup-plugin-styles";
+
+const config = {
+	input: 'src/index.js',
+	output: [
+        {
+            file: 'dist/index.js',
+            format: 'cjs',
+            assetFileNames: "[name][extname]"
+        },
+	    {
+            file: 'dist/index.modern.js',
+            format: 'es',
+            assetFileNames: "[name][extname]"
+        }
+    ],
+    plugins: [
+        peerDepsExternal(),
+        nodeResolve({
+            extensions: ['.js', '.jsx', '.json', '.mjs'],
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            preventAssignment: true
+        }),
+        commonjs(),
+        babel({
+            babelHelpers: 'bundled',
+            exclude: 'node_modules/**',
+            presets: ['@babel/env', '@babel/preset-react']
+        }),
+        json(),
+        styles({
+            mode: 'extract',
+            autoModules: true
+        }),
+        smartAsset({
+            url: 'copy',
+            keepImport: true
+        }),
+    ],
+};
+
+export default config;
