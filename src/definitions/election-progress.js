769cf5597f1e5e221a6e8e5de6ec3e188d60be79
Phil Stewart <p.stewart@imperial.ac.uk>
2023-03-09 12:49:10 +0000

    definitions: Replace 2020 with 2022 in election progress, update UCL to 2022, add le2023csps definition [refs #4056]

--- src/definitions/election-progress.js
+++ src/definitions/election-progress.js
@@ -18,7 +18,12 @@ export const markers = {
   'le2023': [
     { name: '2016', percentage: 45.47, description: '2016: 7966 voters for all positions, 45.47% turnout' },
     { name: '2019', percentage: 41.23, description: '2019: 8121 voters for all positions, 41.23% turnout' },
-    { name: '2020', percentage: 29.57, description: '2020: 6104 voters for all positions, 29.57% turnout' },
-    { name: 'UCL', percentage: 19.58, description: 'UCL (2021): 9473 voters, 19.58% turnout'}
+    { name: '2021', percentage: 27.22, description: '2021: 6212 voters for non-CSP positions, 27.22% turnout' },
+    { name: '2022', percentage: 29.98, description: '2022: 7108 voters for non-CSP positions, 29.98% turnout' },
+    { name: 'UCL', percentage: 21.00, description: 'UCL (2022): 10219 voters, 21.00% turnout'}
+  ],
+  'le2023csps': [
+    { name: '2021', percentage: 27.73, description: '2021: 4754 voters for CSP positions, 27.73% turnout' },
+    { name: '2022', percentage: 30.08, description: '2022: 5595 voters for CSP positions, 30.08% turnout' }
   ],
 }
