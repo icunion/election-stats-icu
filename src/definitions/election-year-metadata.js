2737b3b2606cda9454187120311927f6becb504c
Phil Stewart <p.stewart@imperial.ac.uk>
2024-03-08 19:11:22 +0000

    definitions: Add 2024 year metadata for step-by-step [refs #4185]

--- src/definitions/election-year-metadata.js
+++ src/definitions/election-year-metadata.js
@@ -54,5 +54,33 @@ export const yearData = {
     endTimeThisYear: Date.UTC(2023, 3 - 1, 16, 14, 0, 0),
     maxTime: Date.UTC(2023, 3 - 1, 17, 0, 0, 0),
     votingPeriodText: 'Voting period for Leadership Elections 2023'
+  },
+  'le2024': {
+    years: {
+      '2024': {
+        election: 'le2024',
+        colour: '#390099',
+        offset: 9,
+        zIndex: 2,
+        current: true,
+        periodLength: 81, // 3 days + 5hours and extra 4 hours to end at 18:00
+        actualLength: 77 // 3 days + 5hours
+      },
+      '2023': {
+        election: 'le2023',
+        colour: '#ffa900',
+        offset: 9,
+        zIndex: 1,
+        periodLength: 81,
+        actualLength: 77
+      }
+    },
+    minTime: Date.UTC(2024, 3 - 1, 11, 0, 0, 0),
+    startTime: Date.UTC(2024, 3 - 1, 11, 6, 0, 0),
+    endTime: Date.UTC(2024, 3 - 1, 14, 18, 0, 0),
+    startTimeThisYear: Date.UTC(2024, 3 - 1, 11, 9, 0, 0),
+    endTimeThisYear: Date.UTC(2024, 3 - 1, 14, 14, 0, 0),
+    maxTime: Date.UTC(2024, 3 - 1, 15, 0, 0, 0),
+    votingPeriodText: 'Voting period for Leadership Elections 2024'
   }
 }
