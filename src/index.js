e1cbf4bde9dfd7527658c8c60d0ab559571f2532
Phil Stewart <p.stewart@imperial.ac.uk>
2025-03-10 11:59:18 +0000

    pages/le2025: Copy from le2024

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ export { default as DashboardLE2023 } from './pages/le2023/Dashboard'
 export { default as StepByStepLE2023 } from './pages/le2023/StepByStep'
 export { default as DashboardLE2024 } from './pages/le2024/Dashboard'
 export { default as StepByStepLE2024 } from './pages/le2024/StepByStep'
+export { default as DashboardLE2025 } from './pages/le2025/Dashboard'
+export { default as StepByStepLE2025 } from './pages/le2025/StepByStep'
 export { default as configureStore } from './arithmospora/store'
 
 export * from './hooks'
