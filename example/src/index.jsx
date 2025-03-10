790763f085e9f931f08311031af3bc94dbe87c29
Phil Stewart <p.stewart@imperial.ac.uk>
2025-03-10 12:12:42 +0000

    example: Switch to React 18 createRoot

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,14 +1,15 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store/index'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
-)
+  </Provider>
+);
