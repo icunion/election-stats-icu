9aabc119a1e1cfab87efd137ff66c946f91fb066
Ruben Kretek <rubenkretek@outlook.com>
2026-03-05 09:39:54 +0000

    Removed maxwidth from examples

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -39,22 +39,14 @@ const App = () => {
           cspsSource='le2022csps'
         />
       )}
-      {appToRender === 'DashboardLE2022.maxWidth' && (
-        <DashboardLE2022
-          votingCloseDate={Date.now() + 15000}
-          mainSource='le2022'
-          cspsSource='le2022csps'
-          maxWidth='1170'
-        />
-      )}
-      {appToRender === 'StepByStepLE2022.maxWidth' && (
+
+      {appToRender === 'StepByStepLE2022' && (
         <StepByStepLE2022
           votingCloseDate={Date.now() + 15000}
           mainSource='le2022'
           prevYearSource="le2021"
           thisYear="2022"
           prevYear="2021"
-          maxWidth='1170'
         />
       )}
       {appToRender === 'DashboardLE2023' && (
@@ -64,22 +56,14 @@ const App = () => {
         cspsSource='le2023csps'
       />
       )}
-      {appToRender === 'DashboardLE2023.maxWidth' && (
-        <DashboardLE2023
-        votingCloseDate={Date.now() + 15000}
-        mainSource='le2023'
-        cspsSource='le2023csps'
-        maxWidth='1170'
-        />
-      )}
-      {appToRender === 'StepByStepLE2023.maxWidth' && (
+
+      {appToRender === 'StepByStepLE2023' && (
         <StepByStepLE2023
           votingCloseDate={Date.now() + 15000}
           mainSource='le2023'
           prevYearSource="le2022"
           thisYear="2023"
           prevYear="2022"
-          maxWidth='1170'
         />
       )}
       {appToRender === 'DashboardLE2024' && (
@@ -89,22 +73,14 @@ const App = () => {
         cspsSource='le2024csps'
       />
       )}
-      {appToRender === 'DashboardLE2024.maxWidth' && (
-        <DashboardLE2024
-        votingCloseDate={Date.now() + 15000}
-        mainSource='le2024'
-        cspsSource='le2024csps'
-        maxWidth='1170'
-        />
-      )}
-      {appToRender === 'StepByStepLE2024.maxWidth' && (
+ 
+      {appToRender === 'StepByStepLE2024' && (
         <StepByStepLE2024
           votingCloseDate={Date.now() + 15000}
           mainSource='le2024'
           prevYearSource="le2023"
           thisYear="2024"
           prevYear="2023"
-          maxWidth='1170'
         />
       )}
       {appToRender === 'DashboardLE2025' && (
@@ -114,47 +90,29 @@ const App = () => {
         cspsSource='le2025csps'
       />
       )}
-      {appToRender === 'DashboardLE2025.maxWidth' && (
-        <DashboardLE2025
-        votingCloseDate={Date.now() + 15000}
-        mainSource='le2025'
-        cspsSource='le2025csps'
-        maxWidth='1170'
-        />
-      )}
-      {appToRender === 'StepByStepLE2025.maxWidth' && (
+      {appToRender === 'StepByStepLE2025' && (
         <StepByStepLE2025
           votingCloseDate={Date.now() + 15000}
           mainSource='le2025'
           prevYearSource="le2024"
           thisYear="2025"
           prevYear="2024"
-          maxWidth='1170'
         />
       )}
       {appToRender === 'DashboardLE2026' && (
         <DashboardLE2026
-        votingCloseDate={Date.now() + 15000}
-        mainSource='le2026'
-        cspsSource='le2026csps'
+          votingCloseDate={Date.now() + 15000}
+          mainSource='le2026'
+          cspsSource='le2026csps'
       />
       )}
-      {appToRender === 'DashboardLE2026.maxWidth' && (
-        <DashboardLE2026
-        votingCloseDate={Date.now() + 15000}
-        mainSource='le2026'
-        cspsSource='le2026csps'
-        maxWidth='1170'
-        />
-      )}
-      {appToRender === 'StepByStepLE2026.maxWidth' && (
+      {appToRender === 'StepByStepLE2026' && (
         <StepByStepLE2026
           votingCloseDate={Date.now() + 15000}
           mainSource='le2026'
           prevYearSource="le2025"
           thisYear="2026"
           prevYear="2025"
-          maxWidth='1170'
         />
       )}
     </React.Fragment>
--- example/src/Toolbar.jsx
+++ example/src/Toolbar.jsx
@@ -14,44 +14,18 @@ export const Toolbar = ({ selected, selectHandler, closeHandler }) => {
         <select value={selected} onChange={selectOnChange}>
           <option value='Banner'>Banner</option>
           <option value='Banner.autumn'>Banner Autumn</option>
-          <option value='DashboardLE2026'>DashboardLE2026</option>
-          <option value='DashboardLE2026.maxWidth'>
-            DashboardLE2026 with maxWidth set
-          </option>
-          <option value='StepByStepLE2026.maxWidth'>
-            Step By Step 2026 with maxWidth set
-          </option>
-          <option value='DashboardLE2025'>DashboardLE2025</option>
-          <option value='DashboardLE2025.maxWidth'>
-            DashboardLE2025 with maxWidth set
-          </option>
-          <option value='StepByStepLE2025.maxWidth'>
-            Step By Step 2025 with maxWidth set
-          </option>
-          <option value='DashboardLE2024'>DashboardLE2024</option>
-          <option value='DashboardLE2024.maxWidth'>
-            DashboardLE2024 with maxWidth set
-          </option>
-          <option value='StepByStepLE2024.maxWidth'>
-            Step By Step 2024 with maxWidth set
-          </option>
-          <option value='DashboardLE2023'>DashboardLE2023</option>
-          <option value='DashboardLE2023.maxWidth'>
-            DashboardLE2023 with maxWidth set
-          </option>
-          <option value='StepByStepLE2023.maxWidth'>
-            Step By Step 2023 with maxWidth set
-          </option>
-          <option value='DashboardLE2022'>DashboardLE2022</option>
-          <option value='DashboardLE2022.maxWidth'>
-            DashboardLE2022 with maxWidth set
-          </option>
-          <option value='StepByStepLE2022.maxWidth'>
-            Step By Step with maxWidth set
-          </option>
+          <option value='DashboardLE2026'>2026 Dashboard</option>
+          <option value='StepByStepLE2026'>2026 Step By Step</option>
+          <option value='DashboardLE2025'>2025 Dashboard</option>
+          <option value='StepByStepLE2025'>2025 Step By Step</option>
+          <option value='DashboardLE2024'>2024 Dashboard</option>
+          <option value='StepByStepLE2024'>2024 Step By Step</option>
+          <option value='DashboardLE2023'>2023 Dashboard</option>
+          <option value='StepByStepLE2023'>2023 Step By Step</option>
+          <option value='DashboardLE2022'>2022 Dashboard</option>
+          <option value='StepByStepLE2022'>2022 Step By Step</option>
         </select>
-      </label>
-      {' '}
+      </label>{' '}
       <button onClick={closeHandler}>Hide toolbar</button>
     </div>
   )
